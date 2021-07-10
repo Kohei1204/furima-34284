bf0a85ce621240e8daa78eda26ca63fae902d8a3
Kohei1204 <bigdipper71225@gmail.com>
2021-07-10 23:47:13 +0900

    エラーチェック

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'items/index'
+  get 'items/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   # resources :items, only: [:new, :create] do
   # end
-  resources :users, only: :show
+  # resources :users, only: :show
 end
