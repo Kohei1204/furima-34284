1783734d999391ca16a604298aa96100828a8d84
Kohei1204 <bigdipper71225@gmail.com>
2021-07-11 13:47:20 +0900

    ログアウト機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'items/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
-  resources :items, only: [:new, :create] do
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
   end
   # resources :users, only: :show
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_033942) do
+ActiveRecord::Schema.define(version: 2021_07_01_115018) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
