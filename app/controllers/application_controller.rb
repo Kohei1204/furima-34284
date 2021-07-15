a635b929ddde5dfba4df2d6c8e25833b5346e8cd
Kohei1204 <bigdipper71225@gmail.com>
2021-07-15 20:55:33 +0900

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,6 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :profile])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:lastname, :firstname, :lkananame, :fkananame])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:lastname, :firstname, :lkananame, :fkananame, :birth])
   end
 end
