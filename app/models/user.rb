84bf7e9909dd029a52b16d8e76aea423edf5bc3f
Kohei1204 <bigdipper71225@gmail.com>
2021-07-16 00:22:50 +0900

    必須条件のバリテーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :items
+  has_many :items
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :fkananame, presence: true
+  validates :lkananame, presence: true
+  validates :birth, presence: true
 end
