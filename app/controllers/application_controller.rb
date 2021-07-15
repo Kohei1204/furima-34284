e3c9d2a8311fa86ed95a1bfba543421560bc575e
Kohei1204 <bigdipper71225@gmail.com>
2021-07-16 01:06:48 +0900

    nameへの絡むの修正に伴う変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:lastname, :firstname, :lkananame, :fkananame, :birth])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:lastname, :firstname, :lkananame, :fkananame, :birth])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  validates :nickname, presence: true
+  validates :name, presence: true
   validates :email, presence: true
   validates :encrypted_password, presence: true
   validates :firstname, presence: true
