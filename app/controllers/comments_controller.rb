8a7c0d03c11825dfba43895ce2ffad414bdba3b3
Kohei1204 <bigdipper71225@gmail.com>
2021-07-28 17:09:25 +0900

    コメント機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,8 @@ class CommentsController < ApplicationController
     @comment = Comment.new
   end
   def create
-    @comment = Comment.new(text: params[:comment][:text])
+    @comment = Comment.new(comment_params)
+    # @comment = Comment.new(text: params[:comment][:text])
     if @comment.save
       ActionCable.server.broadcast 'comment_channel', content: @comment
       redirect_to item_path(@comment.item)
