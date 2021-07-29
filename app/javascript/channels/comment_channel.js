35b8ddf5f9334f65c86aa889d8be694c045f5e06
Kohei1204 <bigdipper71225@gmail.com>
2021-07-29 10:16:30 +0900

    コメント機能の修正

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -1,18 +1,12 @@
-// import consumer from "./consumer"
-
-// consumer.subscriptions.create("CommentChannel", {
-//   connected() {
-//     // Called when the subscription is ready for use on the server
-//   },
-
-//   disconnected() {
-//     // Called when the subscription has been terminated by the server
-//   },
-
-//   received(data) {
-//     // Called when there's incoming data on the websocket for this channel
-//     const html = `<p>${data.content.text}</p>`;
-//     const comments = document.getElementById('comments');
-//     const newComment = document.getElementById('comment_text'); comments.insertAdjacentHTML('afterbegin', html); newComment.value='';
-//   }
-// });
+import consumer from "./consumer"
+consumer.subscriptions.create("CommentChannel", { connected() {
+// Called when the subscription is ready for use on the server
+},
+disconnected() {
+// Called when the subscription has been terminated by the server
+},
+received(data) {
+const html = `<p>${data.content.text}</p>`;
+const comments = document.getElementById('comments');
+const newComment = document.getElementById('comment_text'); comments.insertAdjacentHTML('afterbegin', html); newComment.value='';
+} });
